551cbc56e31298a4e1710de746b228b93780f9c2
montini.f@gmail.com <montini.f@gmail.com>
2018-07-30 17:24:52 +0200

    prove

--- index.php
+++ index.php
@@ -2,10 +2,14 @@
 //phpinfo();
 ?>
 
-
+<div id="demo">
+	loading position ...
+</div>
 
 <script>
+
 var x = document.getElementById("demo");
+
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
@@ -13,10 +17,18 @@ function getLocation() {
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
 }
+
 function showPosition(position) {
     x.innerHTML = "Latitude: " + position.coords.latitude + 
     "<br>Longitude: " + position.coords.longitude; 
 }
+
+
+function refresh() {
+	getLocation();
+	setTimeout('refresh()', 1000);	
+}
+
 </script>
 
 
