89fa0336c7e610fdae31ab385aff67686aa6e003
montyclo <montini.f@gmail.com>
2018-08-01 17:42:18 +0200

    procedure allineamneto sorgenti app

--- README.php
+++ README.php
@@ -11,6 +11,27 @@ oc whoami
 oc new-app montyclo:latest~https://github.com/montyclo/geodino.git --name='montyclo'
 
 
+RSH
+---
+
+oc get pods
+oc rsh <pod name>
+
+
+SOURCE SYNC
+-----------
+
+syncronize on github
+
+sync VM openshift:
+oc rsync . app1-1-fxcgr:.
+
+ where app... is the pod listed of the php
+
+
+
+
+
 DATABASE
 --------
 
