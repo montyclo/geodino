8212833afab580b5890c10969d2676f9c01202ca
montyclo <montini.f@gmail.com>
2018-08-02 15:54:03 +0200

    added ffmpeg 64bit support (working) and php mylog function

--- index.php
+++ index.php
@@ -1,18 +1,31 @@
 <?php
 
 include('include/db.php');
+include('include/logs.php');
 
-//echo MYSQL_HOST . ", " . MYSQL_USER . ", " . MYSQL_PASS . ", " . MYSQL_DB . ", " . MYSQL_PORT . "<br>";
+mylog("Trying database connection: " 
+		. MYSQL_HOST . ", " 
+		. MYSQL_USER . ", " 
+		. MYSQL_PASS . ", " 
+		. MYSQL_DB . ", " 
+		. MYSQL_PORT . " ...");
 
-$mysqli = mysqli_connect( MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB, MYSQL_PORT );
+$mysqli = mysqli_connect( 
+		MYSQL_HOST, 
+		MYSQL_USER, 
+		MYSQL_PASS, 
+		MYSQL_DB, 
+		MYSQL_PORT );
 
 // Check connection
 if (mysqli_connect_errno()) {
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	mylog("Failed to connect to MySQL: " . mysqli_connect_error());
 }
 else {
 	mysqli_close( $mysqli );
 	echo "Database connection OK!";
+	mylog("Database connection OK!");
 }
 
 ?>
